d3094d91cebf6272b724812837de03322ebb167c
Zaid Rakhange <zaidcommits.github@gmail.com>
2026-01-11 20:49:53 +0530

    fix : type issues

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,5 @@
+
+import NextAuth from "next-auth";
 declare module "js-yaml" {
   export function load(input: string | Buffer): any;
   export function dump(data: any, options?: any): string;
@@ -6,4 +8,16 @@ declare module "js-yaml" {
 declare module "tar" {
   export function c(options: any, files: string[]): NodeJS.ReadWriteStream;
   export function create(options: any, files: string[]): Promise<void>;
+}
+
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
 }
